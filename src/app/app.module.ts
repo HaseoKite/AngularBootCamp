74aff6ac0fb84c7f9b297dc845d0ef68f302242f
Belinda Murillo <twilight.world@gmail.com>
2024-04-03 01:57:16 -0600

    Homework 7
    Pipes Exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesComponent,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.scss'
+})
+export class PipesComponent {
+  amount: number = 34.22;
+  text: string = "hElLo wOrLd"
+}
