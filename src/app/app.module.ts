5200ad653cd7f0bed6c2fbf5c7c9aa655b1e4b01
Belinda Murillo <twilight.world@gmail.com>
2024-04-02 20:27:46 -0600

    Homework 2
    Structure Directives Exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StructureDirectivesComponent } from './components/structure-directives/structure-directives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StructureDirectivesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/structure-directives/structure-directives.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-directives',
+  templateUrl: './structure-directives.component.html',
+  styleUrl: './structure-directives.component.scss'
+})
+export class StructureDirectivesComponent {
+  characters: string[]= ["A","B","C"]
+}
