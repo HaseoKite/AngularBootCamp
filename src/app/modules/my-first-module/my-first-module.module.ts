c238cd61b4c830ee0d0fc3b19bf858429b4c2aba
Belinda Murillo <twilight.world@gmail.com>
2024-04-02 21:52:43 -0600

    Homework 5
    Modules Exercise

---
+++ src/app/modules/my-first-module/my-first-module.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyFirstRoutingModule } from './my-first-module-routing.module';
+import { MyFirstComponent } from '../../components/my-first-component/my-first-component.component';
+
+
+@NgModule({
+  declarations: [
+    MyFirstComponent
+  ],
+  imports: [
+    CommonModule,
+    MyFirstRoutingModule
+  ]
+})
+export class MyFirstModule { }
