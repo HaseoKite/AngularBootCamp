cc9fb6af318d5300f3237029d1778e26aab329d8
Belinda Murillo <twilight.world@gmail.com>
2024-04-04 01:21:36 -0600

    Homework 9
    Materials Exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialComponent } from './components/material/material.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
